143a8250fce1829b2dd3f7544604917b5a18d4b3
Jamie Stewart <stwjme@gmail.com>
2020-07-17 22:01:08 -0400

    added function

---
+++ fundamentals2.js
@@ -0,0 +1,27 @@
+let arr = [[1, 8, 4, 1], [6, 2 , 7, 12], [21, 104, 19, 200, 7, 9]];
+// first version of programm console.log odd for every odd number 
+// second version console the total number of odd elements
+// console log the odd number is each sub array
+
+// let oddNum = [];
+// let numbers = arr.length;
+
+ 
+  for (let i = 0 ; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      
+      if (arr[i][j] % 2 != 0 ) {
+      // console.log(arr[i][j], 'odd');
+
+      let arr = oddNum;
+
+      if (oddNum < arr.length ++) {
+        let totalNums = arr[i][j] += arr.length;
+        console.log(totalNums);
+
+      }
+      }
+    }
+  }
+
+  
