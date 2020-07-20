afb25c623746db07894ce8acca885101557b9de8
Jamie Stewart <stwjme@gmail.com>
2020-07-20 17:51:42 -0400

    added mutation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Basic Algorithms slice and splice
 Basic Algorithms where do i belong
+Basic Algorithms mutation
---
+++ mutation.js
@@ -0,0 +1,13 @@
+function mutation(arr) {
+
+  let arr2 = arr[1].toLowerCase() ;
+  let arr3 = arr[0].toLowerCase();
+
+  for (var i = 0; i < arr2.length; i++) {
+
+    if (arr3.indexOf(arr2[i]) < 0) return false;
+  }
+  return true;
+}
+
+mutation(["hello", "hey"]);
